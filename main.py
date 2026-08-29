6b35dcda28e6641eddc41c8c17f0fa92dcf17914
pareshmakwana46773-arch <pareshmakwana46773@gmail.com>
2026-08-29 23:33:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from telegram.ext import (
 logging.basicConfig(level=logging.INFO)
 
 # Config
-BOT_TOKEN = "8893475094:AAEXkB_ucaVBFngeTjxhS"
-Qbke fVQLrdRbow"
+BOT_TOKEN = "8893475094:AAEXkB_ucaVBFngeTjxhSQbkefVQLrdRbow"
 TOTAL_SLOTS = 18
 ENTRY_FEE = 18
 WINNER_PERCENTAGE = 0.70
